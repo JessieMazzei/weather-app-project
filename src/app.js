09d8159f4ecb9b82b519ea844c1643a4b79caaeb
JessieMazzei <jessiebampfield@gmail.com>
2023-02-02 21:09:43 -0700

    Linked API for now temp, city name, humidity, feels like, wind speed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#now-temp");
+  let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "5b4a65t5413f55405obca45773f2471a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
